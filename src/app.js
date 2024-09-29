307c8add436dfc7e9545e17bfb1eb8bb90401df4
mgomunist <gomunistmariana@gmail.com>
2024-09-29 20:22:27 +0000

    Excusas añadidas

--- src/app.js
+++ src/app.js
@@ -5,26 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro", "Pedro"];
-let action = ["se comió", "orinó", "aplastó", "rompió"];
-let what = ["mi tarea", "mi teléfono", "el coche"];
+let who = [
+  "Un delfín con esmoquin",
+  "Morgan Freeman",
+  "Dory",
+  "Un pingüino de Madagascar",
+  "Un unicornio"
+];
+let action = [
+  "accidentalmente se tragó a",
+  "hizo una voltereta sobre",
+  "destruyó a",
+  "abofeteó con su cola a",
+  "atacó con un cuerno de unicornio a"
+];
+let what = [
+  "mi router de Wi-Fi",
+  "mis ganas de vivir",
+  "mi autoestima",
+  "Donald Trump",
+  "mi última neurona funcional"
+];
 let when = [
-  "antes de la clase",
-  "cuando estaba durmiendo",
-  "mientras estaba haciendo ejercicio",
-  "durante mi almuerzo",
-  "mientras estaba rezando"
+  "mientras invocaba delfines",
+  "justo cuando estaba descubriendo el sentido de la vida",
+  "durante una escena épica en cámara lenta con arcoíris",
+  "mientras hablaba con un gato sobre física cuántica",
+  "justo antes de que el universo colapsara"
 ];
 
 function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a) + a);
+  return Math.floor(Math.random() * (b - a + 1) + a); // Incluye el último número
 }
 
 function getRandom(anyArray) {
   let max = anyArray.length - 1;
   let min = 0;
-  console;
-  let random = getRandomNumber(min, max);
+  let random = getRandomNumber(min, max); // Asegúrate de que incluya el último elemento
   return anyArray[random];
 }
 window.onload = function() {
