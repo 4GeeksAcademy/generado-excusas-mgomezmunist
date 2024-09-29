cffdf4f7ac4d8ff69ee217f07c02f9db126dfa14
mgomunist <gomunistmariana@gmail.com>
2024-09-29 19:35:40 +0000

    Primer guardado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro", "Pedro"];
+let action = ["se comió", "orinó", "aplastó", "rompió"];
+let what = ["mi tarea", "mi teléfono", "el coche"];
+let when = [
+  "antes de la clase",
+  "cuando estaba durmiendo",
+  "mientras estaba haciendo ejercicio",
+  "durante mi almuerzo",
+  "mientras estaba rezando"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  console;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
